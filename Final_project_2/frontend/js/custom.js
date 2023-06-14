350421fb1aaf34cc329bf6de70be01df632fa711
deimantei <deimantestankunaitemail@gmail.com>
2023-06-14 23:28:02 +0300

    June 14 changes
    Created Filterable Image Gallery and Footer is centered

--- Final_project_2/frontend/js/custom.js
+++ Final_project_2/frontend/js/custom.js
@@ -40,3 +40,21 @@ function contact_function() {
     if ($("#contactform").valid()){
      confirm('Thanks for your message!');}
 }
+
+//Filterable Image Gallery
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterableCards = document.querySelectorAll('.filterable_cards .card');
+
+const filterCards = e => {
+   document.querySelector(".active").classList.remove("active");
+   e.target.classList.add("active");
+
+filterableCards.forEach(card => {
+   card.classList.add('hide');
+   if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === 'all'){
+      card.classList.remove('hide');
+   }
+});
+};
+
+filterButtons.forEach(button => button.addEventListener('click', filterCards));
