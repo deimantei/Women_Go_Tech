7f2a269abc1159dfe31900f4cfd05b4617d549fb
deimantei <deimantestankunaitemail@gmail.com>
2023-06-21 23:05:27 +0300

    Weather Forecast page

---
+++ Weather_forecast/script.js
@@ -0,0 +1,40 @@
+//https://openweathermap.org/current#one
+const apiKey = 'cfc60cf922e31045e5d684e56cf99484';
+
+const submitBtn = document.getElementById('submit-btn');
+const cityInput = document.getElementById('city-input');
+const weatherInfo = document.getElementById('weather-info');
+
+submitBtn.addEventListener('click', () => {
+    const city = cityInput.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(url)
+        .then (response => response.json())
+        .then(data => {
+            const weather = data.weather[0].description;
+            const temperature = Math.round(data.main.temp - 273.15);
+            const clouds = data.clouds.all;
+            const wind = data.wind.speed;
+            //How to set time to that place local time?
+            //const timeZone = (data.timezone / 3600).toString();
+            const time = new Date(data.dt * 1000).toLocaleTimeString();
+            const sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+
+
+            weatherInfo.innerHTML = `
+            <p><strong>City: </strong>${city}</p>
+            <p><strong>Your current time: </strong>${time}</p>
+            <p><strong>Weather: </strong>${weather}</p>
+            <p><strong>Temperature: </strong>${temperature}°C</p>
+            <p><strong>Sunset*: </strong>${sunset}</p>
+            <p><strong>Cloudiness: </strong>${clouds}%</p>
+            <p><strong>Wind speed: </strong>${wind} m/s</p>
+            <p>*in your local time</p>
+            `;
+        })
+        .catch (error => {
+            console.error('Error:', error);
+            weatherInfo.innerHTML = 'An error occurred. Please try again.';
+        });
+});
