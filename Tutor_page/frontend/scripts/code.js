62df4d929bd738e2ab4fac0e0228462e5035c6ee
deimantei <deimantestankunaitemail@gmail.com>
2023-08-22 21:44:33 +0300

    Tutor page
    Tutor page changes - schedule, contact form

--- Tutor_page/frontend/scripts/code.js
+++ Tutor_page/frontend/scripts/code.js
@@ -22,3 +22,24 @@ faders.forEach(fader => {
     appearOnScroll.observe(fader);
 });
 
+//Table
+
+
+//Instagram grid
+
+//contact form
+function sendEmail(){
+    Email.send({
+        SecureToken: "44ad3082-fa63-4100-9f6c-89aac9e0a76b",
+        To : 'ikiegzamino@gmail.com',
+        From:'ikiegzamino@gmail.com',
+        //From : document.getElementById("email").value,
+        Subject : "This is the subject",
+        Body : "Vardas: " + document.getElementById("name").value 
+        + "<br> El. pašto adresas: " + document.getElementById("email").value
+        + "<br> Žinutė: " + document.getElementById("message").value,         
+    }).then(
+      message => alert("Ačiū! Jūsų žinutė išsiųsta.")
+    );
+}
+
